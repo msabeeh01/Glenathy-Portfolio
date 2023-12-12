8180e361179baa5428ac7b7bca7a6170e0a75c25
msabeeh01 <muhammadsabeeh01@gmail.com>
2023-12-12 01:45:10 -0500

    picture update

--- client/src/routes/media/+page.svelte
+++ client/src/routes/media/+page.svelte
@@ -6,7 +6,7 @@
 <div class="flex flex-col place-items-center gap-10 text-center md:flex-row w-[80%] h-full">
 
 		<ImageCardLinked title="Digital" year="Year" image="https://cdn.myportfolio.com/7bf13fc8-be3c-4bd4-96b0-ae0f57e2689c/f29c8721-35bb-49c5-aee4-e2958be0cce3.png?h=5ab12f7f86bce98f81301e35b4a4acc7" href="/digital"/>
-		<ImageCardLinked title="Film" year="Year" image="https://cdn.myportfolio.com/7bf13fc8-be3c-4bd4-96b0-ae0f57e2689c/6b2d2ddd-aa02-4fe8-bb66-9903d1f2f1a4_rwc_125x0x1531x1197x1531.png?h=7f6864a137381bbcf28aae0e32bab52f" href="/film" />
+		<ImageCardLinked title="Film" year="Year" image="https://media.discordapp.net/attachments/917305009270575115/1184004155413639222/Hojean_Film_Edit.jpg?ex=658a64cc&is=6577efcc&hm=e274c922105253b65e100779bf76b0943928a0f964b056e7d02d9c94ab2d2423&=&format=webp&width=1015&height=671" href="/film" />
 
 </div>
 
